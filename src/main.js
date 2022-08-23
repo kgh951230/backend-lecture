c3a0a6b0e6afd022d5f5cccecd68fdb251536d57
kgh951230 <kgh951230@gmail.com>
2022-08-23 15:24:44 +0900

    backend-lecture

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+const express = require('express')
+
+const app = express()
+
+const PORT = 5000
+
+app.use('/', (req, res) => {
+  res.send('Hello express!')
+})
+
+app.listen(PORT, () => {
+  console.log(`The Express server is ${PORT}`)
+})
